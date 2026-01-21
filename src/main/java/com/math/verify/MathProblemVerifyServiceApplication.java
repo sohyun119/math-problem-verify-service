1d99a0467117739a4b7fa12c367eb87488172f08
sohyun119 <sh-119@naver.com>
2026-01-22 00:16:57 +0900

    Initial commit

---
+++ src/main/java/com/math/verify/MathProblemVerifyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.math.verify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathProblemVerifyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathProblemVerifyServiceApplication.class, args);
+    }
+
+}
